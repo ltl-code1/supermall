b435d2ce422348a484f3c477ad564075cd7ac0fc
ltl <ltl_code@163.com>
2020-12-16 16:32:10 +0800

    第一次提交

---
+++ src/network/home.js
@@ -0,0 +1,24 @@
+import { request } from "./request";
+
+export function getHomeMultidata() {
+    return request({
+        url: '/home/multidata'
+    })
+}
+
+export function getHomeGoods(type, page) {
+    return request({
+        url: '/home/data',
+        params: {
+            type,
+            page
+        }
+    })
+}
+
+let totalNums = []
+
+const nums1 = [20, 11, 222]
+const nums2 = [111, 22, 333]
+    //...nums1解析数组
+totalNums.push(...nums1)
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      我的
+  </div>
+</template>
+
+<script>
+  import Scroll from 'components/common/scroll/Scroll'
+
+  export default {
+    name: "Profile",
+    components: {
+      Scroll
+    }
+  }
+</script>
+
+<style scoped>
+  .content {
+    height: 300px;
+    background-color: red;
+    overflow: hidden;
+  }
+</style>
